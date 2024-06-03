e51a0c89bbde6f15388f3b7a64eb6526b32fd79e
Yogesh K <yogeshcodes@zohomail.in>
2024-06-03 18:54:02 +0530

    Add UserStore for db connect and UserCreator interface for methods

--- internal/database/users.go
+++ internal/database/users.go
@@ -1,7 +1,16 @@
 package database
 
+import "database/sql"
+
 type User struct {
 	ID           int
 	Email        string
 	PasswordHash string
+}
+
+type UserCreator interface {
+	Create(name, email string) (*User, error)
+}
+type UserStore struct {
+	DB *sql.DB
 }
