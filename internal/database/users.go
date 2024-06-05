c3dc46fdadfc9893513a7663db099fac251f8c0b
Yogesh K <yogeshcodes@zohomail.in>
2024-06-05 13:54:21 +0530

    Implement Create() user method

--- internal/database/users.go
+++ internal/database/users.go
@@ -1,6 +1,12 @@
 package database
 
-import "database/sql"
+import (
+	"database/sql"
+	"fmt"
+	"strings"
+
+	"golang.org/x/crypto/bcrypt"
+)
 
 type User struct {
 	ID           int
@@ -9,8 +15,35 @@ type User struct {
 }
 
 type UserCreator interface {
-	Create(name, email string) (*User, error)
+	Create(email, password string) (*User, error)
 }
 type UserStore struct {
 	DB *sql.DB
+}
+
+func (us *UserStore) Create(email, password string) (*User, error) {
+	email = strings.ToLower(email)
+
+	hashedBytes, err := bcrypt.GenerateFromPassword(
+		[]byte(password),
+		bcrypt.DefaultCost)
+
+	if err != nil {
+		return nil, fmt.Errorf("create user: %w", err)
+	}
+
+	// write user info to DB
+	row := us.DB.QueryRow(`
+	INSERT INTO users (email, password_hash) 
+	VALUES ($1, $2) RETURNING id`, email, hashedBytes)
+
+	user := User{
+		Email:        email,
+		PasswordHash: string(hashedBytes),
+	}
+	err = row.Scan(&user.ID)
+	if err != nil {
+		return nil, fmt.Errorf("create user: %w", err)
+	}
+	return &user, nil
 }
