eced366467c8f21cec9c7cf3ebac045b3f1b620a
Yogesh K <yogeshcodes@zohomail.in>
2024-06-08 17:15:37 +0530

    Add comments for the functions

--- internal/actions/users.go
+++ internal/actions/users.go
@@ -9,8 +9,13 @@ type Users struct {
 	New Template
 }
 
+// Renders the signup Page (ie. GET /signup)
 func (u Users) NewHandler(w http.ResponseWriter, r *http.Request) {
 	// anonymous struct
+	// This is used to pre fill data in the sign up page
+	// ie when you type in the browser http://localhost:4444/signup?email=bi@bi.io
+	// email was already pre filled and this data is passed to the signup template
+	//page before rendering it
 	data := struct {
 		Email string
 	}{
@@ -19,6 +24,7 @@ func (u Users) NewHandler(w http.ResponseWriter, r *http.Request) {
 	u.New.Execute(w, data)
 }
 
+// This gets called when the signup form is filled and submitted (ie.e POST /signup)
 func (u Users) Create(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Email:  %v\n", r.FormValue("email"))
 	fmt.Fprintf(w, "Password:  %v\n", r.FormValue("password"))
