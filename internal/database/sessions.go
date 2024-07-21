2b7bad41d9ae4fc5be57b660d09a1dea824c4a6f
Yogesh K <yogeshcodes@zohomail.in>
2024-07-21 18:10:50 +0530

    Stubbing session store functions

--- internal/database/sessions.go
+++ internal/database/sessions.go
@@ -1,7 +1,33 @@
 package database
 
+import "database/sql"
+
 type Session struct {
 	ID        int
 	UserID    int
 	TokenHash string
+}
+
+type SessionStore struct {
+	DB *sql.DB
+}
+
+type NewSession struct {
+	NEWSession Session
+	// Token is only set when creating a new session. When looking up a session this will be left empty, as we only store the hash of a session token in our database and we cannot reverse it into a raw token.
+	Token string
+}
+
+//  We will both create the session token and hash it inside Create, then we could return the original session token from the Create method. Create will create a new session for the user provided. The session token will be returned as the Token field on the NEWSession type, but only the hashed session token is stored in the database.
+
+func (ss *SessionStore) Create(userID int) (*NewSession, error) {
+	// TODO: Create the session token
+	// TODO: Implement SessionService.Create
+	return nil, nil
+}
+
+// Once a session is created we will need a way to query our SessionStore to determine who the user is with that session.
+func (ss *SessionStore) UserLookup(token string) (*User, error) {
+	// TODO: Implement SessionService.UserLookup
+	return nil, nil
 }
